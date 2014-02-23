8d42d4969fdf3fc0b6d409872705ccf4ff10f299
gruiz17 <gruiz17@berkeley.edu>
2014-02-23 02:21:59 -0800

    descriptions text file addd

--- scraper.rb
+++ scraper.rb
@@ -8,6 +8,7 @@ yc_page = Nokogiri::HTML(open("http://www.yclist.com/"))
 rows = yc_page.css('tbody tr')
 
 yc_csv = CSV.open("yclist.csv", "w")
+descriptions = File.new("descriptions.txt", "w")
 
 csv_header = ['name', 'url', 'class', 'status', 'description']
 yc_csv << csv_header
@@ -17,6 +18,9 @@ rows.each do |row|
   row.css('td')[1..5].each do |cell|
     if (cell.text != "")
       csv_row << cell.text
+      if (row.css('td')[1..5].index(cell) == 4) && (cell.text != "")
+        descriptions << cell.text + "\n"
+      end
     end
     if (row.css('td')[1..5].index(cell) == 1) && (cell.text == "")
       csv_row << "Unavailable"
